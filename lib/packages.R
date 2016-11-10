67e00791edc3eeca9f9a1e716f12bb2c1b075577
Abhijit Dasgupta <aikidasgupta@gmail.com>
2016-11-10 13:36:07 -0500

    Added reload, packages, AD-specific info

---
+++ lib/packages.R
@@ -0,0 +1,17 @@
+reqpackages <- c('tidyverse',
+                 'stringr',
+                 'reshape2',
+                 'broom',
+                 'ggplot2',
+                 'readxl')
+
+for(pkg in reqpackages){
+  if(!(pkg %in% row.names(installed.packages()))){
+    install.packages(pkg, repos='http://cran.rstudio.com')
+  }
+}
+
+options(defaultPackages = c(getOption('defaultPackages'),reqpackages))
+for(pkg in reqpackages){
+  eval(substitute(suppressMessages(library(x)), list(x=pkg)))
+}
---
+++ preamble.R
@@ -0,0 +1,9 @@
+# Things to pre-load
+#
+
+AD_dirs_mac <- c('data' = '/Volumes/NO NAME/LupusMetaDeath/Abhijit SLE Mortality Library/',
+                  'popdata' = '/Volumes/NO NAME/LupusMetaDeath/',
+                  'home' = '/Users/abhijit/NIAMS/Ward/LupusDeath/')
+
+
+options(dplyr.width=Inf)
