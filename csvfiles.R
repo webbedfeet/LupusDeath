5b4d4ff7e7c2d40367a7460d732bc522dbb081e0
Abhijit Dasgupta <aikidasgupta@gmail.com>
2016-11-20 15:04:00 -0500

    Identified which CSV files are from which type of data

---
+++ csvfiles.R
@@ -0,0 +1,44 @@
+## identifying and adjusting CSV file names to reflect studies
+
+source('lib/reload.R')
+source('preamble.R')
+reload()
+datadir = AD_local_mac['data']
+csvfiles = dir(datadir, pattern='csv')
+if(length(csvfiles)==0 & datadir==AD_dirs_mac['data']) stop("Attach drive")
+csvfiles <- csvfiles[!str_detect(csvfiles,'need')]
+csvfiles <- csvfiles[!str_detect(csvfiles,'child')]
+
+csv_summaries <- csvfiles[str_detect(csvfiles,'summ')]
+csv_cumhaz <- csvfiles[str_detect(csvfiles,'cumhaz')]
+csv_cummort <- csvfiles[str_detect(csvfiles, 'cummort')]
+
+csv_km <- setdiff(csvfiles, c(csv_summaries, csv_cumhaz, csv_cummort))
+
+load('data/rda/study_info.rda')
+has_KM <- ifelse(study_info$KM.fig=='0', 0, 1)
+km_id <- study_info$pubID[has_KM==1]
+
+ids_from_filenames <- csvfiles %>% 
+  str_replace( '-([0-9]{4})','_\\1') %>% 
+  str_extract('^[\\w -]+_[0-9]{4}') %>% 
+  str_to_title(.)
+figs_from_filenames <- csvfiles %>% 
+  str_extract('[Ff]ig[0-9 ]+')
+fig_metadata <- tibble(filename=csvfiles, ids = ids_from_filenames, figs = figs_from_filenames)
+
+## Do we need to pool data
+bl <- fig_metadata %>% count(ids, figs) %>% mutate(needs.pooling=ifelse(n==1,'No','Yes'))
+fig_metadata <- fig_metadata %>% left_join(select(bl, -n))
+
+## Do we have continuous figures
+fig_metadata <- fig_metadata %>% 
+  mutate(cts = ifelse(str_detect(filename, 'cts'),'Yes','No'))
+
+## Which are male-only studies
+fig_metadata <- fig_metadata %>% 
+  mutate(male.only = ifelse(ids %in% study_info$pubID[study_info$male.only=='Y'],'Yes','No'))
+
+## Which studies might represent full studies rather than stratified studies
+fig_metadata <- fig_metadata %>% 
+  mutate(fullstudy = ifelse(ids %in% study_info$pubID[study_info$fullstudy.arm=='Y'], 'Yes','No'))
