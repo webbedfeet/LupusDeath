e28a17c513324a7d1b72fc03b9ebafa21837138c
Abhijit Dasgupta <aikidasgupta@gmail.com>
2016-11-23 17:17:57 -0500

    Added mcmcout

--- scripts/template.py
+++ scripts/template.py
@@ -14,15 +14,15 @@ ind = [int(re.search('([0-9]+)', f).group(1)) for f in datfiles]
 for i in ind:
 	x1 = [u.replace('.mixed',str(i)) for u in x]
 	x1 = [u.replace('Rfile','Rfile'+str(i)) for u in x1]
-	#x1 = [u.replace('Rfile',posixpath.join('//data','dasgupab','lupus',bname,'Rfile')) for u in x1]
+	x1 = [u.replace('Rfile',posixpath.join('//data','dasgupab','lupusdeath',bname,'Rfile')) for u in x1]
 	f = open('Rfile'+str(i)+'.R','w')
 	f.writelines(x1)
 	f.close()
 
-# f = open('Rfile.sw','w')
-# for i in ind:
-# 	f.write('R --vanilla < '+posixpath.join('//data','dasgupab','lupus',bname,'Rfile')+str(i)+'.R\n')
-# f.close()
+f = open('Rfile.sw','w')
+for i in ind:
+	f.write('R --vanilla < '+posixpath.join('//data','dasgupab','lupusdeath',bname,'Rfile')+str(i)+'.R\n')
+f.close()
 
-#shutil.copy(os.path.join(localdir,'fullmodelcts.bug'),'.')
-#shutil.copy(os.path.join(localdir,'fullmodelcts2.bug'),'.')
+# shutil.copy(os.path.join(localdir,'fullmodelcts.bug'),'.')
+# shutil.copy(os.path.join(localdir,'fullmodelcts2.bug'),'.')
