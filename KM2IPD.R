9a3c1033f596b9f60f8cbd184182124a8b51ec0f
Abhijit Dasgupta <aikidasgupta@gmail.com>
2016-11-23 16:04:37 -0500

    Updated files to compute the IPDs

--- KM2IPD.R
+++ KM2IPD.R
@@ -5,7 +5,7 @@ load('data/rda/KM_digitized.rda') #loads KM_full & KM_digitized_clean
 
 KM2stratIPD <- list()
 for(u in names(KM_digitized_clean)){
-  KM2stratIPD[[u]] <- get.IPD(KM_digitized_clean[[u]], 
+  KM2stratIPD[[u]] <- get_IPD(KM_digitized_clean[[u]], 
                          KM_full$N[KM_full$filename==u])
 }
 
@@ -13,7 +13,7 @@ for(u in names(KM_digitized_clean)){
 KM2IPD <- list()
 for(u in KM_full$ids){
   lst <- KM2stratIPD[KM_full$filename[KM_full$ids==u]]
-  KM2IPD[[u]] <- pool.ipd(lst)
+  KM2IPD[[u]] <- pool_ipd(lst)
   KM2IPD[[u]] <- lapply(KM2IPD[[u]], unname)
 }
 save(KM2IPD, file='data/rda/KM2IPD.rda', compress=T)
