7ff0889a509792b75a270206368d3c17281bbe05
Abhijit Dasgupta <aikidasgupta@gmail.com>
2016-11-25 14:33:09 -0500

    Updated beginYear to conform with study_info

--- lib/R/beginYear.R
+++ lib/R/beginYear.R
@@ -2,9 +2,9 @@
 #' 
 #' Compute the start year of each study, in case it is missing
 beginYear <- function(dat=study_info){
-  starts <- dat$start
+  starts <- dat$start.enrollment
   ind <- which(is.na(starts))
-  starts[ind] <- with(dat[ind,], round(pubdate - pmax(maxfollowup, esrdfu..m.,na.rm=T)/12-1))
+  starts[ind] <- with(dat[ind,], round(pubdate - pmax(as.numeric(max.f.up), f.up.months,na.rm=T)/12-1))
   ind <- which(is.na(starts)) # just arms of 118
   starts[ind] <- starts[min(ind)-1]
   return(starts)
