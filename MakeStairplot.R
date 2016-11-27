1fb4326a51c824c370972a6507d4b7bebd87c3e4
Abhijit Dasgupta <aikidasgupta@gmail.com>
2016-11-27 16:55:51 -0500

    Added code in stairplot to clean duplicates

--- MakeStairplot.R
+++ MakeStairplot.R
@@ -4,9 +4,21 @@ source('lib/reload.R')
 reload()
 load('data/rda/study_info.rda')
 
+filter_overall <- function(d){
+  if(any(str_detect(d$Arm, '[abc]$'))){
+    d <- filter(d, Arm!='')
+  }
+  return(d)
+}
+
 info <- study_info %>% filter(armID==pubID)
 info <- rbind(info,
-              study_info %>% filter(!(pubID %in% info$pubID)))
+              study_info %>% filter(!(pubID %in% info$pubID))) %>% 
+  nest(-Author) %>% 
+  mutate(data = map(data, ~filter_overall(.))) %>% 
+  unnest()
+
+info <- info %>% mutate(pubID = pubID %>% str_replace('_',' (') %>% paste0(')') )
 
 plt <- info %>% 
   mutate(yr_of_study_end = end_of_study) %>% 
