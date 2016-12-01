291df12c07617a7d361b6f267f7b3cf2505d9c70
Abhijit Dasgupta <aikidasgupta@gmail.com>
2016-12-01 15:24:37 -0500

    Made MovingAvgResults reproducible
    Added rda files

--- MovingAvgResults.R
+++ MovingAvgResults.R
@@ -1,6 +1,8 @@
 # Extracting results from moving average analysis
 source('lib/reload.R')
 reload()
+load('data/rda/window_membership.rda')
+load('data/rda/study_info.rda')
 
 plt <- 'adult' %>% mcmcout() %>% 
   collapseResults() %>% 
@@ -9,3 +11,5 @@ plt <- 'adult' %>% mcmcout() %>%
 pdf(file='graphs/adultMA.pdf')
 print(plt)
 dev.off()
+
+
