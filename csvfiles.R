646eef0a3faca5f5ba684756a538014c9b61c53f
Abhijit Dasgupta <aikidasgupta@gmail.com>
2016-12-13 20:56:09 -0500

    Added metadata and incorporated it in csvfiles.R
    Added whether time is measured in years and if time starts
    at SLE diagnosis. This is incorporated into fig_metadata

--- csvfiles.R
+++ csvfiles.R
@@ -5,7 +5,7 @@ source('preamble.R')
 reload()
 datadir = AD_local_mac['data']
 csvfiles = dir(datadir, pattern='csv')
-if(length(csvfiles)==0 & datadir==AD_dirs_mac['data']) stop("Attach drive")
+# if(length(csvfiles)==0 & datadir==AD_dirs_mac['data']) stop("Attach drive")
 csvfiles <- csvfiles[!str_detect(csvfiles,'need')]
 csvfiles <- csvfiles[!str_detect(csvfiles,'child')]
 
@@ -25,10 +25,12 @@ ids_from_filenames <- csvfiles %>%
   str_to_title(.)
 figs_from_filenames <- csvfiles %>% 
   str_extract('[Ff]ig[0-9 ]+')
-fig_metadata <- tibble(filename=csvfiles, ids = ids_from_filenames, figs = figs_from_filenames)
+fig_metadata <- tibble(filename=csvfiles, ids = ids_from_filenames, 
+                       figs = figs_from_filenames)
 
 ## Do we need to pool data
-bl <- fig_metadata %>% dplyr::count(ids, figs) %>% mutate(needs.pooling=ifelse(n==1,'No','Yes'))
+bl <- fig_metadata %>% dplyr::count(ids, figs) %>% 
+  mutate(needs.pooling=ifelse(n==1,'No','Yes'))
 fig_metadata <- fig_metadata %>% left_join(select(bl, -n))
 
 ## ID true KM files
@@ -38,6 +40,32 @@ fig_metadata <- fig_metadata %>%
 # ## Which are male-only studies
 # fig_metadata <- fig_metadata %>% 
 #   mutate(male.only = ifelse(ids %in% study_info$pubID[study_info$male.only=='Y'],'Yes','No'))
+
+## Some meta-data on the png files
+## We need to see if curves started at diagnosis, and 
+## ensure that time is recorded in months
+pngfiles = dir(datadir, pattern='png')
+pngfiles %>% 
+  str_replace('-([0-9]{4})','_\\1') %>% 
+  str_extract('^[\\w -]+_[0-9]{4}[abc]?') %>% 
+  str_to_title() -> png_id
+png <- tibble(pngfiles, png_id)
+png %>% 
+  rbind(data.frame(pngfiles = rep('Urman-1977-fig1.png',2), 
+                   png_id = paste0('Urman_1977',c('a','b')))) %>% 
+  filter(png_id != 'Urman_1977')-> png
+
+bl = fig_metadata %>% left_join(png, by=c('ids'='png_id'))
+bl$pngfiles[bl$filename=='Ballou-1982-fig2-summ.csv'] <- 'Ballou-1982-fig2.png'
+bl$pngfiles[bl$filename=='Ballou-1982-fig3-summ.csv'] <- 'Ballou-1982-fig3.png'
+bl <- distinct(bl)
+# write.csv(bl, file='pngfiles.csv', row.names=F) # Add meta-data to this file
+# don't write this file again...metadata is already included
+
+png_info <- read_csv('pngfiles.csv') %>% 
+  mutate(`from SLE` = ifelse(is.na(`from SLE`), 'n','y'),
+         years = ifelse(is.na(years),'y','n'))
+fig_metadata <- left_join(fig_metadata, png_info)
 
 
 save(fig_metadata, file='data/rda/fig_metadata.rda')
--- preamble.R
+++ preamble.R
@@ -1,12 +1,12 @@
 # Things to pre-load
 #
 
-AD_dirs_mac <- c('data' = '/Volumes/NO NAME/LupusMetaDeath/Abhijit SLE Mortality Library/',
-                  'popdata' = '/Volumes/NO NAME/LupusMetaDeath/',
-                  'home' = '/Users/abhijit/NIAMS/Ward/LupusDeath/')
-AD_local_mac <- c('data' = '/Users/abhijit/Dropbox/Work/Ward/Studies/LupusMetaDeath/Abhijit SLE Mortality Library/',
-                  'popdata' = '/Users/abhijit/Dropbox/Work/Ward/Studies/LupusMetaDeath/',
-                  'home' = '/Users/abhijit/NIAMS/Ward/LupusDeath/')
+AD_dirs_mac <- c('data' = '/Volumes/NO NAME/LupusMetaDeath/Abhijit SLE Mortality Library',
+                  'popdata' = '/Volumes/NO NAME/LupusMetaDeath',
+                  'home' = '/Users/abhijit/NIAMS/Ward/LupusDeath')
+AD_local_mac <- c('data' = '/Users/abhijit/Dropbox/Work/Ward/Studies/LupusMetaDeath/Abhijit SLE Mortality Library',
+                  'popdata' = '/Users/abhijit/Dropbox/Work/Ward/Studies/LupusMetaDeath',
+                  'home' = '/Users/abhijit/NIAMS/Ward/LupusDeath')
 FH_dirs <- c('data'='Papers/Abhijit SLE Mortality Library/',
              'popdata' = 'Papers')
 AD_peds_dirs <- c('data'= '/Users/abhijit/Dropbox/Work/Ward/Studies/LupusMetaDeath/pediatric')
