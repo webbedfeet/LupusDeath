cf49d489595118082fdc4cd0b5eb7e249d6f5fb2
Abhijit Dasgupta <aikidasgupta@gmail.com>
2016-12-20 13:33:24 -0500

    Finished creating summ2IPD

--- SummarySurvival.R
+++ SummarySurvival.R
@@ -17,6 +17,7 @@ fig_metadata <- fig_metadata %>%
   mutate(male.only = ifelse(ids %in% study_info$pubID[study_info$male.only=='Y'],'Yes','No'))
 
 metadata <- fig_metadata %>% filter(str_detect(filename, 'summ')) %>% 
+  filter(!str_detect(filename, 'cummort')) %>% 
   filter(male.only=='No')
 Ns <- study_info$number[study_info$pubID %in% metadata$ids]
 names(Ns) <- study_info$pubID[study_info$pubID %in% metadata$ids]
@@ -33,6 +34,14 @@ metadata$N[is.na(metadata$N)] <- c(
   499, 40, # Wang
   330, 117, 25 # Wu
 )
+bl = study_info %>% filter(pubID %in% unique(metadata$ids)) %>% 
+  filter(armID==pubID) %>% 
+  select(pubID, armID, number, deaths, max.f.up, start_of_study, end_of_study) %>% 
+  mutate(max.f.up = round(max.f.up/12), 
+         max.f.up = ifelse(is.na(max.f.up), end_of_study - start_of_study, max.f.up), 
+         n.death = round(deaths*number/100))
+metadata <- metadata %>% left_join(bl %>% select(pubID, n.death, max.f.up), by=c('ids' = 'pubID')) %>% 
+  mutate(n.death = ifelse(needs.pooling=='Yes', NA, n.death))
 
 save(metadata, file = 'data/rda/SummSurvMetadata.rda') 
 
@@ -54,13 +63,33 @@ csvdata <- lapply(csvdata, Summ_clean)
 id <- 'Hersh-2010-fig1-adult-summ.csv'
 csvdata[[id]] <- mutate(csvdata[[id]], Time=Time/12) # This data is in months
 
+# Create IPD from possibly stratified graphical summaries -----------------
 
-# Import summary data not in graphs ---------------------------------------
+IPD_from_csv_stratified <- list()
+for(n in names(csvdata)){
+  print(n)
+  summData <- csvdata[[n]]
+  params <- as.list(weibEst(summData))
+  meta <- metadata %>% filter(filename==n)
+  IPD_from_csv_stratified[[n]] <- 
+    summ2IPD_2(summData, params,
+               meta$N,
+               meta$n.death,
+               meta$max.f.up)
+}
+
+IPD_from_csv <- list()
+for(u in metadata$ids){
+  lst = IPD_from_csv_stratified[metadata$filename[metadata$ids==u]]
+  IPD_from_csv[[u]] <- pool_ipd(lst)
+  IPD_from_csv[[u]] <- lapply(IPD_from_csv[[u]],unname)
+}
+
+# Import summary data not in graphs --------------------------------------- 
 load('data/rda/graph_types.rda')
 ids <- c(csv_km, csv_summaries) %>% str_extract('^[\\w -]+[0-9]{4}[abc]?') %>% 
   str_replace('-([0-9]{4})','_\\1') %>% str_to_title()
 
-'%notin%' <- Negate('%in%')
 
 clean_summaries <- function(d){
   d <- rbind(tibble(Time=0, Prob=100), d) %>% 
@@ -70,7 +99,8 @@ clean_summaries <- function(d){
 dat <- study_info %>% filter(pubID %!in% ids) %>% 
 select(armID, pubID, starts_with('surv')) %>% 
 gather(Time, Prob, starts_with('surv')) %>% 
-mutate(Time = str_extract(Time, '[0-9]+')) %>% 
+mutate(Time = str_extract(Time, '[0-9]+'),
+       Time = as.numeric(Time)) %>% 
 filter(!is.na(Prob)) %>% 
 arrange(armID)
 dat <- dat[dat$pubID %!in% (study_info %>% filter(male.only=='Y') %>% select(pubID) %>% as_vector),] %>% 
@@ -79,9 +109,29 @@ nest(-pubID) %>%
 mutate(data = map(data, ~clean_summaries(.)))
 summ_data <- as.list(dat$data); names(summ_data) <- dat$pubID
 
+metadata_for_spreadsheet <- study_info %>% 
+  filter(pubID %in% names(summ_data)) %>% 
+  filter(armID==pubID) %>% 
+  select(pubID, number, deaths, max.f.up, start_of_study, end_of_study) %>% 
+  mutate(n.death = round(number*deaths/100),
+         max.f.up = round(max.f.up/12),
+         max.f.up = ifelse(is.na(max.f.up), end_of_study-start_of_study, max.f.up))
 
-# Weibull estimation ------------------------------------------------------
+IPD_from_spreadsheet <- list()
+for(u in names(summ_data)){
+  print(u)
+  summData <- summ_data[[u]]
+  summData[1,] <- c(0.001,0.999)
+  params <- as.list(weibEst(summData))
+  meta <- metadata_for_spreadsheet %>% filter(pubID==u)
+  IPD_from_spreadsheet[[u]] <- 
+    summ2IPD_2(summData, params,
+               meta$number,
+               meta$n.death,
+               meta$max.f.up)
+}
 
+# Pool all IPD and store --------------------------------------------------
 
-param <- weib_param(csvdata[[n1]], metadata$Lag[metadata$filename==n1])
- 
+summaries2IPD <- c(IPD_from_csv, IPD_from_spreadsheet)
+save(summaries2IPD, file='data/rda/summaries2IPD.rda', compress=T)
