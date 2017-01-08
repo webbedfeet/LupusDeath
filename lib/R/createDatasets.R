17b18df4adacd3896045c0a383376e956e72bd78
Abhijit Dasgupta <aikidasgupta@gmail.com>
2017-01-08 15:55:37 -0500

    Updated createDatasets to include followup studies

--- lib/R/createDatasets.R
+++ lib/R/createDatasets.R
@@ -1,18 +1,18 @@
 #' @title Create datasets in a folder that are required for the Moving Average estimation
 #' @author Abhijit Dasgupta <abhijit.dasgupta@nih.gov>
-#' 
+#'
 #' @description
 #' This function creates JAGS-compatible datasets and adds required files to a new folder for a particular
-#' Moving Average analysis based on annual windows. It adds IPD data from the studies which are members 
+#' Moving Average analysis based on annual windows. It adds IPD data from the studies which are members
 #' of each window into a JAGS-compatible file.
-#' 
+#'
 #' @param members A tidy table for which study contributes to which window
 #' @param  outdir Directory to store the files
 #' @param  info Study information (including studi ID, class, developed status and left truncation info)
 #' @param  minkm=5 Minimum number of Kaplan-Meier studies to be included per window
 #' @param ipd=study.ipd A list of study-specific IPD, derived from KM curves
 #' @param  followup=followup.fullstudy data.frame with followup information (max followup, events, number at risk, etc) (currently not implemented)
-createDatasets <- function(members, ipd, outdir, info=study_info, minkm=5, followup=NULL){
+createDatasets <- function(members, ipd, outdir, info=study_info, minkm=5, followup=fup_data){
   outdir <- file.path('data','mcmc',outdir)
   if(file.exists(outdir)) unlink(outdir, recursive=T)
   dir.create(outdir, showWarnings=FALSE, recursive=T) # ensure directory exists and is empty
@@ -20,10 +20,9 @@ createDatasets <- function(members, ipd, outdir, info=study_info, minkm=5, follo
   for(i in 1:(ncol(members1)-1)){
     print(i)
     if(sum(members1[,i+1]) < minkm) next # need at least minkm KM curves present in window
-    # ind2 <- intersect(as.numeric(members[[i]]), followup$study)
     ipd1 <- ipd[members1[,i+1]]
-    # follow <- dplyr::filter(followup, study%in% ind2)
-    jagsdata <- datForJags(ipd1,follow=NULL,info=info)
+    followup1 <- followup %>% dplyr::filter(pubID %in% members$pubID[members[,i+1]])
+    jagsdata <- datForJags(ipd1,follow=followup1,info=info)
     if(!is.null(jagsdata)){
       assign(paste('data',i,sep=''), jagsdata)
       dump(paste('data',i,sep=''), file=file.path(outdir,paste('fullmodel',i,'.txt',sep='')))
--- lib/R/datForJags.R
+++ lib/R/datForJags.R
@@ -1,45 +1,54 @@
 #' Creating data for import into JAGS
-#' 
-#' This function inputs data from the survival studies, including IPD data and 
+#'
+#' This function inputs data from the survival studies, including IPD data and
 #' followup data, and converts it into the R dump() format for import into
-#' JAGS 
-#' 
+#' JAGS
+#'
 #' @param ipd This is a list of IPD data, one per study. Each element is
 #'            itself a list comprising 2 elements, t.d (death) and t.cens (censored) times
-#' 
-#' @param  follow A data.frame that contains information from studies that 
+#'
+#' @param  follow A data.frame that contains information from studies that
 #'                contains only followup time and number of events, in addition
 #'                to some study characteristics (not currently implemented)
 #' @param  info A data.frame that contains information about each study (study_info for this study)
 #' @return A list in R dump() format using all the input data that is amenable for
 #'         processing by JAGS
-#' 
-datForJags <- function(ipd, follow=NULL, info=study_info){
+#'
+datForJags <- function(ipd, follow=fup_data, info=study_info){
   # out <- follow[,c('number','maxfollowup','Events','Developed',
   #                  'lags')]
   dat.jags <- as.data.frame(gen_jagsdata(ipd,info))
-  # names(out)[c(1,4)] <- c('n','geog2')
-  # out$isCensored2 <- as.integer(rep(1,nrow(out)))
-  # out$Y <- rep(NA, nrow(out))
-  #   out$yr2 <- as.factor(out$yr2)
-  # out$geog2 <- as.factor(out$geog2)
-  
+  names(dat.jags) <- c('td','tcens','trunc','isCensored1','geog1')
+  dat.jags$geog1 <- as.factor(dat.jags$geog1)
+  dat.jags <- as.list(dat.jags)
+  dat.jags$N1 <- nrow(dat.jags)
+
+# Follow-up data
+  if(!is.null(follow)){
+  out <- follow %>% select(max.f.up, n.death, number, Developed, Lag) %>%
+    rename(maxfollowup = max.f.up, geog2=Developed, n = number, Events=n.death) %>%
+    mutate(isCensored2 = as.integer(rep(1,n()))) %>%
+    mutate(Y = rep(NA, n())) %>%
+    mutate(geog2 = as.factor(geog2))
+  dat_follow <- as.list(out)
+  dat_follow$N2 <- nrow(out)
+  dat <- c(dat.jags, dat_follow)
+  dat$J <- length(unique(c(dat$geog1, dat$geog2)))
+  } else {
+    dat <- dat.jags
+    dat$N2 <- 0
+    dat$J <- length(unique(dat$geog1))
+  }
   # dat <- as.list(out)
   # dat$N2 <- nrow(out)
-  names(dat.jags) <- c('td','tcens','trunc','isCensored1','geog1')
   # dat.jags <- dat.jags[,-6]
   #   dat.jags$yr1 <- as.factor(dat.jags$yr1)
-  dat.jags$geog1 <- as.factor(dat.jags$geog1)
-  
+
   # dat <- c(dat, as.list(dat.jags))
-  dat <- as.list(dat.jags)
-  dat$N1 <- nrow(dat.jags)
-  if(is.null(follow)) dat$N2 <- 0
+  # dat$J <- length(unique(c(dat$geog1,dat$geog2)))
+  #   dat$K <- length(unique(c(dat$yr1,dat$yr2)))
   for(i in 1:length(dat)){
     if(is.factor(dat[[i]])) dat[[i]] <- as.numeric(dat[[i]])
   }
-  # dat$J <- length(unique(c(dat$geog1,dat$geog2)))
-  dat$J <- length(unique(c(dat$geog1)))
-  #   dat$K <- length(unique(c(dat$yr1,dat$yr2)))
   return(dat)
 }
