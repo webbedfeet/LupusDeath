01f22f5e2f31c2e99018e6bf60bf999e982ad289
Abhijit Dasgupta <aikidasgupta@gmail.com>
2017-01-27 15:48:41 -0500

    Updated weibEst to keep all valid times, resolves #32

--- lib/R/weibEst.R
+++ lib/R/weibEst.R
@@ -1,6 +1,6 @@
 weibEst <- function(d){
-  d <- d %>% filter(Time > 0, Time <=15)
-  m <- lm(log(-log(Prob))~log(Time), data=d[-1,])
+  d <- d %>% filter(Time > 0, Time <=20) # Removes Time=0 to keep log computable
+  m <- lm(log(-log(Prob))~log(Time), data=d)
   out = c(shape = m$coef[2], scale = exp(-m$coef[1]/m$coef[2]))
   names(out) <- c('shape','scale')
   return(out)
