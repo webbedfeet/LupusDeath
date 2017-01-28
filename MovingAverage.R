d88459546d0453c7acf3bd282b3654c12a3b45ee
Abhijit Dasgupta <aikidasgupta@gmail.com>
2017-01-27 19:44:14 -0500

    Made priors more diffuse based on parametrization

--- MovingAverage.R
+++ MovingAverage.R
@@ -16,8 +16,8 @@ model{
 # Prior model & estimated quantities
 for(j in 1:J){
 #beta[j] ~ dnorm(0.0,0.0001);
-nu[j] ~ dgamma(1.0,0.0001);
-lambda[j] ~ dgamma(1.0,0.0001);
+nu[j] ~ dgamma(1.0,10)
+lambda[j] ~ dgamma(1.0,10);
 pr5[j] <- 1-pweib(5.0, nu[j],lambda[j]);
 pr10[j] <- 1-pweib(10.0, nu[j],lambda[j]);
 pr15[j] <- 1-pweib(15.0, nu[j],lambda[j]);
@@ -43,8 +43,8 @@ model{
 # Prior model & estimated quantities
 for(j in 1:J){
 #beta[j] ~ dnorm(0.0,0.0001);
-nu[j] ~ dgamma(1.0,0.0001);
-lambda[j] ~ dgamma(1.0,0.0001);
+nu[j] ~ dgamma(1.0,10);
+lambda[j] ~ dgamma(1.0,10);
 pr5[j] <- 1-pweib(5.0, nu[j],lambda[j]);
 pr10[j] <- 1-pweib(10.0, nu[j],lambda[j]);
 pr15[j] <- 1-pweib(15.0, nu[j],lambda[j]);
@@ -71,5 +71,25 @@ fig_metadata <- fig_metadata %>%
 ipds <- KM2IPD[setdiff(names(KM2IPD), fig_metadata$ids[fig_metadata$male.only=='Yes'])]
 ipds <- c(ipds,
           summaries2IPD[setdiff(names(summaries2IPD), study_info$pubID[study_info$male.only=='Y'])])
+
 ## Use study_info since summaries2IPD contains data from spreadsheet, not just graphical
+
+## Remove suspicious study which is killing JAGS, Manger_2002. This does solve problem.
+ipds <- ipds[-which(names(ipds)=='Manger_2002')]
 createDatasets(membership, ipds, outdir='adult')
+
+
+#### Why is Manger_2002 a problem. We'll look at everything in the window
+# load('data/rda/window_membership.rda')
+# win <- membership %>% select(pubID, Window39:Window50)
+# win$n = apply(win[,-1], 1, sum)
+# win <- win %>% filter(n==12)
+# ids = win$pubID
+# ids = intersect(ids, names(ipds))
+# bl <- ipds[ids]
+# bl2 <- lapply(bl, function(x){
+#   sfit = tidy(summary(survfit(IPD2Surv(x)~1), times=0:20)) %>% select(time,Survival)
+#   return(sfit)
+# })
+# bl3 = plyr::ldply(bl2, .id='pubID') %>% mutate(sz = ifelse(pubID=='Manger_2002',2,1))
+# ggplot(bl3, aes(time, Survival,group=pubID, color=pubID))+geom_line(aes(size=as.integer(sz)))
