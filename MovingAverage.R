dad4626475df5eb56a6287f2d0313144478a7538
Abhijit Dasgupta <aikidasgupta@gmail.com>
2017-01-30 00:48:14 -0500

    Returned jags code to old model

--- MovingAverage.R
+++ MovingAverage.R
@@ -16,8 +16,9 @@ model{
 # Prior model & estimated quantities
 for(j in 1:J){
 #beta[j] ~ dnorm(0.0,0.0001);
-nu[j] ~ dgamma(1.0,10)
-lambda[j] ~ dgamma(1.0,10);
+ nu[j] ~ dgamma(3.0,3.0);
+lambda[j] ~ dgamma(3.0,0.03);
+#lambda[j] ~ dunif(20,200);
 pr5[j] <- 1-pweib(5.0, nu[j],lambda[j]);
 pr10[j] <- 1-pweib(10.0, nu[j],lambda[j]);
 pr15[j] <- 1-pweib(15.0, nu[j],lambda[j]);
@@ -31,7 +32,7 @@ td[i] ~ dweib(nu[geog1[i]], lambda[geog1[i]])T(trunc[i],);
 for(i in 1:N2){
 p[i] <- pweib(maxfollowup[i]+Lag[i], nu[geog2[i]], lambda[geog2[i]])-pweib(Lag[i],nu[geog2[i]], lambda[geog2[i]]);
 isCensored2[i] ~ dinterval(Y[i], Events[i]);
-Y[i] ~ dbinom(p[i], n[i]);
+Y[i] ~ dbin(p[i], n[i]);
 }
 }
 "
@@ -43,8 +44,9 @@ model{
 # Prior model & estimated quantities
 for(j in 1:J){
 #beta[j] ~ dnorm(0.0,0.0001);
-nu[j] ~ dgamma(1.0,10);
-lambda[j] ~ dgamma(1.0,10);
+nu[j] ~ dgamma(3.0,3.0);
+lambda[j] ~ dgamma(3.0,0.03);
+#lambda[j] ~ dunif(50,200);
 pr5[j] <- 1-pweib(5.0, nu[j],lambda[j]);
 pr10[j] <- 1-pweib(10.0, nu[j],lambda[j]);
 pr15[j] <- 1-pweib(15.0, nu[j],lambda[j]);
@@ -75,7 +77,7 @@ ipds <- c(ipds,
 ## Use study_info since summaries2IPD contains data from spreadsheet, not just graphical
 
 ## Remove suspicious study which is killing JAGS, Manger_2002. This does solve problem.
-ipds <- ipds[-which(names(ipds)=='Manger_2002')]
+# ipds <- ipds[-which(names(ipds)=='Manger_2002')]
 createDatasets(membership, ipds, outdir='adult')
 
 load('data/rda/window_membership_10.rda')
@@ -91,17 +93,3 @@ membership_inception <- membership %>% filter(pubID %in% inception_ids)
 createDatasets(membership_inception, ipds_inception, outdir='inception', minkm=3,
                followup = fup_data %>% filter(pubID %in% inception_ids))
 
-#### Why is Manger_2002 a problem. We'll look at everything in the window
-# load('data/rda/window_membership.rda')
-# win <- membership %>% select(pubID, Window39:Window50)
-# win$n = apply(win[,-1], 1, sum)
-# win <- win %>% filter(n==12)
-# ids = win$pubID
-# ids = intersect(ids, names(ipds))
-# bl <- ipds[ids]
-# bl2 <- lapply(bl, function(x){
-#   sfit = tidy(summary(survfit(IPD2Surv(x)~1), times=0:20)) %>% select(time,Survival)
-#   return(sfit)
-# })
-# bl3 = plyr::ldply(bl2, .id='pubID') %>% mutate(sz = ifelse(pubID=='Manger_2002',2,1))
-# ggplot(bl3, aes(time, Survival,group=pubID, color=pubID))+geom_line(aes(size=as.integer(sz)))
