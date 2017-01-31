b99500bd631a6baadd60126461fbd6a143331c21
Abhijit Dasgupta <aikidasgupta@gmail.com>
2017-01-31 13:37:21 -0500

    Created code for panel of results plots

--- MovingAvgResults.R
+++ MovingAvgResults.R
@@ -4,12 +4,15 @@ reload()
 load('data/rda/window_membership.rda')
 load('data/rda/study_info.rda')
 
-plt <- 'adult' %>% mcmcout() %>% 
-  collapseResults() %>% 
+titles <- c('adult' = 'All data',
+            'adult_10' = 'Restricted to 10 years',
+            'inception' = 'Inception cohorts')
+pdf(file = 'graphs/adultMA.pdf')
+for(x in paste('rda',c('adult','adult_10','inception'), sep='/')){
+plt <- x %>% mcmcout() %>%
+  collapseResults() %>%
   pltResults()
-
-pdf(file='graphs/adultMA.pdf')
-print(plt)
+print(plt+ggtitle(titles[str_replace(x, 'rda/','')]))
+}
 dev.off()
-
 
