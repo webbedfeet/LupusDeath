4afea784ee8bee26931484fff89c3fc6660d3d88
Abhijit Dasgupta <aikidasgupta@gmail.com>
2017-01-31 13:33:28 -0500

    Updated to specify dplyr::count

--- lib/R/gen_jagsdata.R
+++ lib/R/gen_jagsdata.R
@@ -1,5 +1,5 @@
 #' Converting a list of IPD into JAGS-compatible data
-#' 
+#'
 #' @param ipd The list of IPDs
 #' @param info Other data, matched on some index with the names of the IPD list, that will be included
 #'            in the JAGS dataset. Typically this is a data.frame of clinical characteristics of interest
@@ -10,9 +10,9 @@ gen_jagsdata <- function(ipd, info=study_info){
     ipd[[i]]$pubID <- names(ipd)[i]
   }
   info <- dplyr::filter(info, armID==pubID)
-  tst <- info %>% count(pubID)
+  tst <- info %>% dplyr::count(pubID)
   if(any(tst$n > 1))stop('Need single info row per study')
-  
+
   data.jags <- dplyr::bind_rows(lapply(ipd, function(d) jags_transform(d, info)))
   return(as.list(data.jags))
 }
