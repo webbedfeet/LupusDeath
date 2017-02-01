1ac0e5b8b075b318c0f2d7174f34bf1800138e77
Abhijit Dasgupta <dasgupab@mail.nih.gov>
2017-01-31 19:09:47 -0500

    Added code to automate OS issues

--- preamble.R
+++ preamble.R
@@ -7,10 +7,22 @@ AD_dirs_mac <- c('data' = '/Volumes/NO NAME/LupusMetaDeath/Abhijit SLE Mortality
 AD_local_mac <- c('data' = '/Users/abhijit/Dropbox/Work/Ward/Studies/LupusMetaDeath/Abhijit SLE Mortality Library',
                   'popdata' = '/Users/abhijit/Dropbox/Work/Ward/Studies/LupusMetaDeath',
                   'home' = '/Users/abhijit/NIAMS/Ward/LupusDeath')
-FH_dirs <- c('data'='Papers/Abhijit SLE Mortality Library/',
+AD_local_win <- AD_local_mac %>% str_replace('/Users','C:/Users') %>% str_replace('abhijit','dasgupab') %>%
+  str_replace('NIAMS/','')
+names(AD_local_win) <- names(AD_local_mac)
+
+if(Sys.info()['sysname'] == 'Windows'){
+  datadir <- AD_local_win['data']
+  popdir <- AD_local_win['popdata']
+} else {
+  popdir <- AD_local_mac['popdata']
+  datadir <- AD_local_mac['data']
+}
+
+FH_dirs <- c('data' = 'Papers/Abhijit SLE Mortality Library/',
              'popdata' = 'Papers')
-AD_peds_dirs <- c('data'= '/Users/abhijit/Dropbox/Work/Ward/Studies/LupusMetaDeath/pediatric')
-options(dplyr.width=Inf, stringsAsFactors=F)
+AD_peds_dirs <- c('data' = '/Users/abhijit/Dropbox/Work/Ward/Studies/LupusMetaDeath/pediatric')
+options(dplyr.width = Inf, stringsAsFactors=F)
 
 `%!in%` <- Negate(`%in%`)
 
