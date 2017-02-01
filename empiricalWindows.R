4c7fa9c2bca6601f4c9e348e29122e0e79d640f7
Abhijit Dasgupta <aikidasgupta@gmail.com>
2017-01-31 19:43:53 -0500

    Added possibility of different membership and ipd inputs

--- empiricalWindows.R
+++ empiricalWindows.R
@@ -9,7 +9,7 @@ load('data/rda/window_membership.rda')
 ipds <- c(KM2IPD, summaries2IPD)
 members <- membership %>% filter(pubID %in% names(ipds))
 
-draw_curves <- function(n, by_id=T, by_dev = !by_id){ # n is window number in memberships
+draw_curves <- function(n, members, ipds, by_id=T, by_dev = !by_id){ # n is window number in memberships
   ids <- members$pubID[members[[paste0('Window',n)]]]
   lapply(ipds[ids], function(ipd){
     tidy(summary(survfit(IPD2Surv(ipd)~1),times = seq(0,20,by=0.5)))
