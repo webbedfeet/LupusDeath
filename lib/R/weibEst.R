043c4a6b94fabc7babacb1af67487b2389759ad9
Abhijit Dasgupta <dasgupab@mail.nih.gov>
2017-01-31 19:10:37 -0500

    Removed 0 values in weibEst

--- lib/R/weibEst.R
+++ lib/R/weibEst.R
@@ -1,5 +1,5 @@
 weibEst <- function(d){
-  d <- d %>% filter(Time > 0, Time <=20) # Removes Time=0 to keep log computable
+  d <- d %>% dplyr::filter(Time > 0, Time <=20) # Removes Time=0 to keep log computable
   m <- lm(log(-log(Prob))~log(Time), data=d)
   out = c(shape = m$coef[2], scale = exp(-m$coef[1]/m$coef[2]))
   names(out) <- c('shape','scale')
