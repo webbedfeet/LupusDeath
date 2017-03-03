67380f07f3a49f7dd2d74a9c2e78fd9315313d96
Abhijit Dasgupta <dasgupab@mail.nih.gov>
2017-03-03 16:29:18 -0500

    Updated to just produce 10 year graphs

--- MakeStairplot.R
+++ MakeStairplot.R
@@ -13,31 +13,29 @@ filter_overall <- function(d){
 
 # Remove male-only studies and Grigor
 
-info <- study_info %>% filter(armID==pubID)
-info <- rbind(info,
-              study_info %>% filter(!(pubID %in% info$pubID))) %>%
-  nest(-Author) %>%
-  mutate(data = map(data, ~filter_overall(.))) %>%
-  unnest() %>%
-  filter(male.only=='N') %>%
-  filter(pubID != 'Grigor_1978')
+info <- study_info %>% filter(armID==pubID) %>% filter(male.only=='N')
+# info <- rbind(info,
+#               study_info %>% filter(!(pubID %in% info$pubID))) %>%
+  # filter(male.only=='N') %>%
+  # filter(pubID != 'Grigor_1978') ## Mike has no info on this 3/3/17
 
+## Just present 10 year truncation since the graphs will contain all the information
 plt <- info %>%
   mutate(pubID = pubID %>% str_replace('_',' (') %>% paste0(')')) %>%
-  mutate(yr_of_study_end = end_of_study) %>%
+  mutate(yr_of_study_end = end_of_study_10) %>%
   stairdata() %>%
   stairplot() + ggtitle('Overall')
 
 plt_developed <- info %>%
   mutate(pubID = pubID %>% str_replace('_',' (') %>% paste0(')'),
-         yr_of_study_end = end_of_study) %>%
+         yr_of_study_end = end_of_study_10) %>%
   filter(Developed=='Developed') %>%
   stairdata() %>%
   stairplot()+ggtitle('Developed countries')
 
 plt_developing <- info %>%
   mutate(pubID = pubID %>% str_replace('_',' (') %>% paste0(')'),
-         yr_of_study_end = end_of_study) %>%
+         yr_of_study_end = end_of_study_10) %>%
   filter(Developed=='Developing') %>%
   stairdata() %>%
   stairplot() + ggtitle('Developing countries')
@@ -47,13 +45,13 @@ print(plt_developed)
 print(plt_developing)
 dev.off()
 
-plt2 <- info %>%
-  mutate(pubID = pubID %>% str_replace('_',' (') %>% paste0(')') ) %>%
-  mutate(yr_of_study_end = end_of_study_10) %>%
-  stairdata() %>%
-  stairplot()
-
-
-pdf(file = 'graphs/stairplot_adult_10yr.pdf')
-print(plt2)
-dev.off()
+# plt2 <- info %>%
+#   mutate(pubID = pubID %>% str_replace('_',' (') %>% paste0(')') ) %>%
+#   mutate(yr_of_study_end = end_of_study_10) %>%
+#   stairdata() %>%
+#   stairplot()
+#
+#
+# pdf(file = 'graphs/stairplot_adult_10yr.pdf')
+# print(plt2)
+# dev.off()
