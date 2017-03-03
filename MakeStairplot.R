3f9913c96142fd278d18402b8524839452ac4a6e
Abhijit Dasgupta <aikidasgupta@gmail.com>
2017-03-03 13:37:17 -0500

    Updated stairplot generator

--- MakeStairplot.R
+++ MakeStairplot.R
@@ -2,7 +2,7 @@
 
 source('lib/reload.R')
 reload()
-load('data/rda/study_info.rda')
+load('data/rda/final_study_info.rda')
 
 filter_overall <- function(d){
   if(any(str_detect(d$Arm, '[abc]$'))){
@@ -15,45 +15,45 @@ filter_overall <- function(d){
 
 info <- study_info %>% filter(armID==pubID)
 info <- rbind(info,
-              study_info %>% filter(!(pubID %in% info$pubID))) %>% 
-  nest(-Author) %>% 
-  mutate(data = map(data, ~filter_overall(.))) %>% 
-  unnest() %>% 
-  filter(male.only=='N') %>% 
+              study_info %>% filter(!(pubID %in% info$pubID))) %>%
+  nest(-Author) %>%
+  mutate(data = map(data, ~filter_overall(.))) %>%
+  unnest() %>%
+  filter(male.only=='N') %>%
   filter(pubID != 'Grigor_1978')
 
-plt <- info %>% 
-  mutate(pubID = pubID %>% str_replace('_',' (') %>% paste0(')')) %>% 
-  mutate(yr_of_study_end = end_of_study) %>% 
-  stairdata() %>% 
+plt <- info %>%
+  mutate(pubID = pubID %>% str_replace('_',' (') %>% paste0(')')) %>%
+  mutate(yr_of_study_end = end_of_study) %>%
+  stairdata() %>%
   stairplot() + ggtitle('Overall')
 
-plt_developed <- info %>% 
-  mutate(pubID = pubID %>% str_replace('_',' (') %>% paste0(')'), 
-         yr_of_study_end = end_of_study) %>% 
-  filter(Developed=='Developed') %>% 
-  stairdata() %>% 
+plt_developed <- info %>%
+  mutate(pubID = pubID %>% str_replace('_',' (') %>% paste0(')'),
+         yr_of_study_end = end_of_study) %>%
+  filter(Developed=='Developed') %>%
+  stairdata() %>%
   stairplot()+ggtitle('Developed countries')
 
-plt_developing <- info %>% 
-  mutate(pubID = pubID %>% str_replace('_',' (') %>% paste0(')'), 
-         yr_of_study_end = end_of_study) %>% 
-  filter(Developed=='Developing') %>% 
-  stairdata() %>% 
+plt_developing <- info %>%
+  mutate(pubID = pubID %>% str_replace('_',' (') %>% paste0(')'),
+         yr_of_study_end = end_of_study) %>%
+  filter(Developed=='Developing') %>%
+  stairdata() %>%
   stairplot() + ggtitle('Developing countries')
-pdf(file='graphs/stairplot.pdf')
+pdf(file='graphs/stairplot_adult.pdf')
 print(plt)
 print(plt_developed)
 print(plt_developing)
 dev.off()
 
-plt2 <- info %>% 
-  mutate(pubID = pubID %>% str_replace('_',' (') %>% paste0(')') ) %>% 
-  mutate(yr_of_study_end = end_of_study_10) %>% 
-  stairdata() %>% 
+plt2 <- info %>%
+  mutate(pubID = pubID %>% str_replace('_',' (') %>% paste0(')') ) %>%
+  mutate(yr_of_study_end = end_of_study_10) %>%
+  stairdata() %>%
   stairplot()
 
 
-pdf(file = 'graphs/stairplot_10yr.pdf')
+pdf(file = 'graphs/stairplot_adult_10yr.pdf')
 print(plt2)
 dev.off()
